25ff01f79649a3246fbccc6cc9fa1983ac48953f
Realmunene <onlinemunene@gmail.com>
2025-12-02 11:53:56 +0300

    resolving config assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local = false
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
-  config.assets.compile = false
+  # config.assets.compile = false
   config.log_level = :info
   config.log_tags = [:request_id]
   config.active_storage.service = :local
